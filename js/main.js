297d60342da0692345e96c35e10beb3d4994265d
Sasha20055 <nikolalex783@gmail.com>
2023-05-18 19:28:27 +0300

    slide

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('.slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    appendArrows: $('.content'),
+    speed: 1000,
+    autoplay: true,
+  })
+
+}) 
